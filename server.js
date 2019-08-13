7c33487641034fcfd13d137c38a4e93b32c9068d
Dannyyzz <dannyzz@live.com>
2019-08-12 22:10:32 -0400

    Update route for server use of post

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const posts = require("./routes/posts");
 
-const posts = require('./routes/posts');
 
 const server = express();
 
 server.use(express.json()); 
 
-server.use('/posts', posts);
+server.use('/api/posts', posts);
 
 module.exports = server;
